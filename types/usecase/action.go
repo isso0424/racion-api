89c415c5024fa4ca41c387c6c112bcc03da09889
Kota Isozaki <49902177+isso0424@users.noreply.github.com>
2021-03-16 05:17:22 +0900

    feat: add new usecase (#12)

--- types/usecase/action.go
+++ types/usecase/action.go
@@ -7,6 +7,7 @@ import (
 
 type ActionInteractor interface {
 	Create(title, color string, tags []string, startAt, endAt time.Time) domain.Action
+	CreateFromTemplate(title string, template domain.Template, startAt, endAt time.Time) domain.Action
 	Edit(title, color string, tags []string) domain.Action
 	GetAll() []domain.Action
 	GetByTitle() domain.Action
