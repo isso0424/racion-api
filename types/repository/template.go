374f727be7d99b5dc558c1fcbfd22f63f97a12a8
isso0424 <isso04240129@gmail.com>
2021-03-17 23:55:20 +0900

    fix: update usecase with id

--- types/repository/template.go
+++ types/repository/template.go
@@ -4,7 +4,8 @@ import "isso0424/racion-api/types/domain"
 
 type TemplateRepository interface {
 	Create(name, color string, tags []domain.Tag) (domain.Template, error)
-	Edit(name, color string, tags []domain.Tag) (domain.Template, error)
+	Edit(id, name, color string, tags []domain.Tag) (domain.Template, error)
 	GetAll() ([]domain.Template, error)
 	GetByName(name string) (domain.Template, error)
+	GetByID(id string) (domain.Template, error)
 }
