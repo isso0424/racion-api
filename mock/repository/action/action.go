c1db62d9a0d307d27cc69d2a4c1b4cba677a9d06
isso0424 <isso04240129@gmail.com>
2021-03-19 03:19:05 +0900

    feat: impl action mock repository

--- mock/repository/action/action.go
+++ mock/repository/action/action.go
@@ -1,6 +1,7 @@
 package action
 
 import (
+	"errors"
 	"isso0424/racion-api/types/domain"
 	"time"
 )
@@ -10,21 +11,58 @@ type MockActionDB struct {
 }
 
 func(db *MockActionDB) Create(title, color string, tags []domain.Tag, startAt, endAt time.Time) (domain.Action, error) {
-	return domain.Action{}, nil
+	newData := domain.Action{
+		Title: title,
+		Color: color,
+		Tags: tags,
+		StartAt: startAt,
+		EndAt: endAt,
+	}
+
+	db.Data = append(db.Data, newData)
+
+	return newData, nil
 }
 
 func(db *MockActionDB) Edit(id, title, color string, tags []domain.Tag, startAt, endAt time.Time) (domain.Action, error) {
-	return domain.Action{}, nil
+	for index, data := range db.Data {
+		if data.ID == id {
+			db.Data[index].Title = title
+			db.Data[index].Color = color
+			db.Data[index].Tags = tags
+			db.Data[index].StartAt = startAt
+			db.Data[index].EndAt = endAt
+
+			return db.Data[index], nil
+		}
+	}
+
+	return domain.Action{}, errors.New("target not found")
 }
 
 func(db MockActionDB) GetAll() ([]domain.Action, error) {
-	return []domain.Action{}, nil
+	return db.Data, nil
 }
 
 func(db MockActionDB) GetByTitle(title string) ([]domain.Action, error) {
-	return []domain.Action{}, nil
+	var result []domain.Action
+	for _, data := range db.Data {
+		if data.Title == title {
+			result = append(result, data)
+		}
+	}
+
+	if len(result) != 0 {
+		return result, nil
+	}
+	return []domain.Action{}, errors.New("target not found")
 }
 
 func(db MockActionDB) GetByID(id string) (domain.Action, error) {
-	return domain.Action{}, nil
+	for _, data := range db.Data {
+		if data.ID == id {
+			return data, nil
+		}
+	}
+	return domain.Action{}, errors.New("target not found")
 }
