760aeca4e0737c3038590885c8936331cc369124
isso0424 <isso04240129@gmail.com>
2021-03-20 05:20:49 +0900

    feat: action edit controller

--- types/usecase/action.go
+++ types/usecase/action.go
@@ -8,7 +8,7 @@ import (
 type ActionInteractor interface {
 	Create(title, color string, tags []string, startAt, endAt time.Time) (domain.Action, error)
 	CreateFromTemplate(title, templateID string, startAt, endAt time.Time) (domain.Action, error)
-	Edit(id, title, color string, tags []string) (domain.Action, error)
+	Edit(id, title, color string, tags []string, startAt, endAt time.Time) (domain.Action, error)
 	GetAll() ([]domain.Action, error)
 	GetByTitle(title string) (domain.Action, error)
 	GetByID(id string) (domain.Action, error)
