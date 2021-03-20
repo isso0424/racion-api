efad27d48383142ee014d875aa242cf00eedade6
isso0424 <isso04240129@gmail.com>
2021-03-21 05:21:38 +0900

    feat: edit controller

---
+++ controller/template/edit.go
@@ -0,0 +1,17 @@
+package template
+
+import "isso0424/racion-api/types/domain"
+
+func(controller TemplateController) Edit(id, name, color string, tags []string) (domain.Template, error) {
+	var tagsArray []domain.Tag
+	for _, tagID := range tags {
+		tag, err := controller.tagRepo.GetByID(tagID)
+		if err != nil {
+			return domain.Template{}, err
+		}
+
+		tagsArray = append(tagsArray, tag)
+	}
+
+	return controller.templateRepo.Edit(id, name, color, tagsArray)
+}
