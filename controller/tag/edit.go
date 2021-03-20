dddac05f7c4f905ce43608bcf6fda2382c202cbc
isso0424 <isso04240129@gmail.com>
2021-03-21 01:43:38 +0900

    feat: edit controller

---
+++ controller/tag/edit.go
@@ -0,0 +1,7 @@
+package tag
+
+import "isso0424/racion-api/types/domain"
+
+func(repo TagContoller) Edit(title, description, color string) (domain.Tag, error) {
+	return repo.Edit(title, description, color)
+}
