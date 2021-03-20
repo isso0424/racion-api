51fd8b74266986d5af649eaf6eb6e1f65857e38d
isso0424 <isso04240129@gmail.com>
2021-03-21 06:55:23 +0900

    feat: get template controller

---
+++ controller/template/get.go
@@ -0,0 +1,15 @@
+package template
+
+import "isso0424/racion-api/types/domain"
+
+func(controller TemplateController) GetAll() ([]domain.Template, error) {
+	return controller.templateRepo.GetAll()
+}
+
+func(controller TemplateController) GetByName(name string) ([]domain.Template, error) {
+	return controller.templateRepo.GetByName(name)
+}
+
+func(controller TemplateController) GetByID(id string) (domain.Template, error) {
+	return controller.templateRepo.GetByID(id)
+}
--- controller/template/template_test.go
+++ controller/template/template_test.go
@@ -21,7 +21,7 @@ func setup() controller.TemplateController {
 					{
 						Title: "tag1",
 						Description: "desc",
-						ID: "id",
+						ID: "tag_id",
 						Color: "#ffffff",
 					},
 				},
@@ -34,7 +34,7 @@ func setup() controller.TemplateController {
 			{
 				Title: "tag1",
 				Description: "desc",
-				ID: "id",
+				ID: "tag_id",
 				Color: "#ffffff",
 			},
 		},
@@ -45,20 +45,53 @@ func setup() controller.TemplateController {
 
 func TestCreate(t *testing.T) {
 	ctrl := setup()
-	template, err := ctrl.Create("test", "color", []string{"id"})
+	template, err := ctrl.Create("test", "color", []string{"tag_id"})
 
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "test", template.Name)
 	assert.Equal(t, "color", template.Color)
-	assert.Equal(t, "id", template.Tags[0].ID)
+	assert.Equal(t, "tag_id", template.Tags[0].ID)
 }
 
 func TestEdit(t *testing.T) {
 	ctrl := setup()
-	template, err := ctrl.Edit("id", "edited", "color", []string{"id"})
+	template, err := ctrl.Edit("id", "edited", "color", []string{"tag_id"})
 
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "edited", template.Name)
 	assert.Equal(t, "color", template.Color)
-	assert.Equal(t, "id", template.Tags[0].ID)
+	assert.Equal(t, "tag_id", template.Tags[0].ID)
+}
+
+func TestGet(t *testing.T) {
+	ctrl := setup()
+	templates, err := ctrl.GetAll()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "template1", templates[0].Name)
+	assert.Equal(t, "#000000", templates[0].Color)
+	assert.Equal(t, "id", templates[0].ID)
+	assert.Equal(t, "tag_id", templates[0].Tags[0].ID)
+
+	templates, err = ctrl.GetByName("template1")
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "template1", templates[0].Name)
+	assert.Equal(t, "#000000", templates[0].Color)
+	assert.Equal(t, "id", templates[0].ID)
+	assert.Equal(t, "tag_id", templates[0].Tags[0].ID)
+
+	template, err := ctrl.GetByID("id")
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "template1", template.Name)
+	assert.Equal(t, "#000000", template.Color)
+	assert.Equal(t, "id", template.ID)
+	assert.Equal(t, "tag_id", templates[0].Tags[0].ID)
+}
+
+func TestFail(t *testing.T) {
+	ctrl := setup()
+	_, err := ctrl.Create("hoge", "fuga", []string{"invalid"})
+	assert.NotEqual(t, nil, err)
+
+	_, err = ctrl.Edit("invalid", "hoge", "fuga", []string{"invalid"})
+	assert.NotEqual(t, nil, err)
 }
