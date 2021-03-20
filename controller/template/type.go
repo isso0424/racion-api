f033833bee43c6976571a0618b0ec299a4f00d0a
isso0424 <isso04240129@gmail.com>
2021-03-21 03:22:54 +0900

    feat: define template controller

---
+++ controller/template/type.go
@@ -0,0 +1,12 @@
+package template
+
+import "isso0424/racion-api/types/repository"
+
+type TemplateController struct {
+	tagRepo repository.TagRepository
+	templateRepo repository.TemplateRepository
+}
+
+func New(tagRepo repository.TagRepository, templateRepo repository.TemplateRepository) TemplateController {
+	return TemplateController{ tagRepo, templateRepo }
+}
