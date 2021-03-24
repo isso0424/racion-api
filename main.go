b468049789fe71929d7adcd9f7d79a01a4b0d888
isso0424 <isso04240129@gmail.com>
2021-03-25 00:37:46 +0900

    feat: create get tag route

--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 	"isso0424/racion-api/mock/repository/template"
 	"isso0424/racion-api/router/variables"
 	"isso0424/racion-api/server"
+	"isso0424/racion-api/types/domain"
 	"log"
 )
 
 func main() {
-	variables.New(&action.MockActionDB{}, &tag.MockTagDB{}, &template.MockTemplateDB{})
+	variables.New(
+		&action.MockActionDB{ Data: make([]domain.Action, 0) },
+		&tag.MockTagDB{ Data: make([]domain.Tag, 0) },
+		&template.MockTemplateDB{ Data: make([]domain.Template, 0) },
+	)
 	log.Println(server.Serve(&server.Config{ListenIP: "localhost", ListenPort: 8000}))
 }
---
+++ router/tag/get.go
@@ -0,0 +1,112 @@
+package tag
+
+import (
+	"isso0424/racion-api/router/handler"
+	"isso0424/racion-api/router/logger"
+	"isso0424/racion-api/router/responser"
+	"isso0424/racion-api/router/variables"
+	"net/http"
+)
+
+type Get struct {}
+
+func(route Get) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	type Query struct {
+		Title string
+		ID string
+	}
+	query := Query{}
+
+	err := variables.Decoder.Decode(&query, r.URL.Query())
+	if err != nil {
+		handler.HandleError(err.Error(), "internal server error", http.StatusInternalServerError, route, w)
+
+		return
+	}
+
+	if query.ID != "" && query.Title != "" {
+		errMessage := "Cannot use more than one args"
+		handler.HandleError(errMessage, errMessage, http.StatusBadRequest, route, w)
+
+		return
+	}
+
+	if query.ID != "" {
+		tag, err := variables.TagController.GetByID(query.ID)
+		if err != nil {
+			handler.HandleError(err.Error(), "not found", http.StatusNotFound, route, w)
+
+			return
+		}
+
+		err = responser.Success(
+			responser.DonePayload{
+				Status: http.StatusOK,
+				Data: tag,
+				Route: route,
+			},
+			w,
+		)
+		if err != nil {
+			logger.LoggingError(route, err.Error())
+		}
+
+		return
+	}
+
+	if query.Title != "" {
+		tags, err := variables.TagController.GetByTitle(query.Title)
+		if err != nil {
+			handler.HandleError(err.Error(), "not found", http.StatusNotFound, route, w)
+
+			return
+		}
+
+		err = responser.Success(
+			responser.DonePayload{
+				Status: http.StatusOK,
+				Data: tags,
+				Route: route,
+			},
+			w,
+		)
+		if err != nil {
+			logger.LoggingError(route, err.Error())
+		}
+
+		return
+	}
+
+	tags, err := variables.TagController.GetAll()
+	if err != nil {
+		handler.HandleError(err.Error(), "internal server error", http.StatusInternalServerError, route, w)
+
+		return
+	}
+
+	err = responser.Success(
+		responser.DonePayload{
+			Status: http.StatusOK,
+			Data: tags,
+			Route: route,
+		},
+		w,
+	)
+	if err != nil {
+		logger.LoggingError(route, err.Error())
+
+		return
+	}
+}
+
+func(route Get) Name() string {
+	return "get tags"
+}
+
+func(route Get) Path() string {
+	return "/tag"
+}
+
+func(route Get) Method() string {
+	return "GET"
+}
