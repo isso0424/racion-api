3c15516e02e65f281ee397be1fde8afcf6a989b7
isso0424 <isso04240129@gmail.com>
2021-03-26 09:04:48 +0900

    feat: create creating template route

--- router/routes/routes.go
+++ router/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"isso0424/racion-api/router"
 	"isso0424/racion-api/router/tag"
+	"isso0424/racion-api/router/template"
 )
 
 var Routes = []router.Route{
@@ -10,4 +11,5 @@ var Routes = []router.Route{
 	tag.Get{},
 	tag.Put{},
 	tag.Delete{},
+	template.Create{},
 }
---
+++ router/template/create.go
@@ -0,0 +1,73 @@
+package template
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"isso0424/racion-api/router/handler"
+	"isso0424/racion-api/router/logger"
+	"isso0424/racion-api/router/responser"
+	"isso0424/racion-api/router/variables"
+	"net/http"
+)
+
+type Create struct {}
+
+func(route Create) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	type Param struct {
+		Tags []string
+		Name string
+		Color string
+	}
+	param := Param{}
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		handler.HandleError(err.Error(), "internal server error", http.StatusInternalServerError, route, w)
+
+		return
+	}
+
+	err = json.Unmarshal(data, &param)
+	if err != nil {
+		handler.HandleError(err.Error(), "internal server error", http.StatusInternalServerError, route, w)
+
+		return
+	}
+
+	if param.Name == "" || param.Color == "" {
+		handler.HandleError("invalid arguments", "invalid arguments", http.StatusBadRequest, route, w)
+
+		return
+	}
+
+	template, err := variables.TemplateController.Create(param.Name, param.Color, param.Tags)
+	if err != nil {
+		handler.HandleError(err.Error(), "internal server error", http.StatusInternalServerError, route, w)
+
+		return
+	}
+
+	err = responser.Success(
+		responser.DonePayload{
+			Data: template,
+			Status: http.StatusCreated,
+			Route: route,
+		},
+		w,
+	)
+	if err != nil {
+		logger.LoggingError(route, err.Error())
+	}
+}
+
+func(route Create) Name() string {
+	return "create template"
+}
+
+func(route Create) Path() string {
+	return "/template"
+}
+
+func(route Create) Method() string {
+	return "POST"
+}
