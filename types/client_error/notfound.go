b96401f14338f6539fc3f508266819fe2a0fd9b9
isso0424 <isso04240129@gmail.com>
2021-03-26 10:13:18 +0900

    feat: create not found type

---
+++ types/client_error/notfound.go
@@ -0,0 +1,27 @@
+package client_error
+
+import "fmt"
+
+type notFound struct {
+	Target string
+	Type string
+	Condition string
+}
+
+func(err notFound) Error() string {
+	return fmt.Sprintf("%s(%s, %s) not found", err.Type, err.Target, err.Condition)
+}
+
+func CreateNotFound(t, target, condition string) notFound {
+	return notFound{ Target: target, Type: t, Condition: condition }
+}
+
+func IsNotFound(err error) bool {
+	errMsg := err.Error()
+	length := len(errMsg)
+	if length < 9 {
+		return false
+	}
+
+	return errMsg[length - 9 : length] == "not found"
+}
